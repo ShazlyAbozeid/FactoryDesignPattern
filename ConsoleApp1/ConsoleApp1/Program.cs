e1adae86caf6599fcf3af4caa141f0931c1236c3
sabozeid <shazly.abozeid@medstreaming.com>
2020-12-14 17:58:43 +0200

    adding project files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generally we will get the Card Type from UI.
+            //Here we are hardcoded the card type
+            string cardType = "MoneyBack";
+            CreditCard cardDetails = null;
+            //Based of the CreditCard Type we are creating the
+            //appropriate type instance using if else condition
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+
+        }
+
+    }
+
+    public interface CreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+    class MoneyBack : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+
+    public class Titanium : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+    }
+    public class Platinum : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+    }
+}
